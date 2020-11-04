2864ebbba940fd0e1770acee731a7c3e260f26c6
marcioforte <68228009+marcioforte@users.noreply.github.com>
2020-11-04 10:34:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://treinamentomarciodev.herokuapp.com/validarcpf?cpf="
+const URL="https://treinamentomarciodev.herokuapp.com"
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
